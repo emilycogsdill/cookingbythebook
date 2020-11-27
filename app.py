6fc0008f030cc95d26f92ae5cfc75bb6741282d4
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-27 10:06:04 -0600

    recipify

--- app.py
+++ app.py
@@ -5,31 +5,35 @@ app = Flask(__name__)
 application = app
 
 # create a list of dicts from a CSV
-presidents_list = convert_to_dict("presidents.csv")
+recipes_list = convert_to_dict("recipe_info.csv")
 
 # create a list of tuples in which the first item is the number
-# (Presidency) and the second item is the name (President)
+# (id) and the second item is the recipe name (Title)
 pairs_list = []
-for p in presidents_list:
-    pairs_list.append( (p['Presidency'], p['President']) )
+for p in recipes_list:
+    pairs_list.append( (p['id'], p['Title']) )
 
 # first route
 
 @app.route('/')
 def index():
-    return render_template('index.html', pairs=pairs_list, the_title="Presidents Index")
+    return render_template('index.html', pairs=pairs_list, the_title="Recipes Index")
 
 # second route
 
-@app.route('/president/<num>')
-def detail(num):
+@app.route('/recipe/<id>')
+def detail(id):
     try:
-        pres_dict = presidents_list[int(num) - 1]
+        recipe_dict = recipes_list[int(id) - 1]
     except:
-        return f"<h1>Invalid value for Presidency: {num}</h1>"
+        return f"<h1>Invalid recipe id: {id}</h1>"
     # a little bonus function, imported on line 2 above
-    ord = make_ordinal( int(num) )
-    return render_template('president.html', pres=pres_dict, ord=ord, the_title=pres_dict['President'])
+    ord = make_ordinal( int(id) )
+    return render_template('recipe.html'
+                           , recipe=recipe_dict
+                           , ord=ord
+                           , the_title=recipe_dict['Title']
+                          )
 
 
 # keep this as is
