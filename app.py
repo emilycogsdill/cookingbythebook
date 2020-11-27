055f37b69873ac10b1b200e65dca65208e7097a7
Emily Cogsdill <47834372+emilycogsdill@users.noreply.github.com>
2020-11-27 15:49:59 -0600

    add fancy form stuff

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
-from modules import convert_to_dict, make_ordinal
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from modules import * 
 
 app = Flask(__name__)
-application = app
+
+# Flask-WTF requires an encryption key - the string can be anything
+app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
+
+# Flask-Bootstrap requires this line
+Bootstrap(app)
+
+# with Flask-WTF, each web form is represented by a class
+# "NameForm" can change; "(FlaskForm)" cannot
+# see the route for "/" and "index.html" to see how this is used
+class NameForm(FlaskForm):
+    name = StringField('Which recipe do you want to look up?', validators=[DataRequired()])
+    submit = SubmitField('Go fish (but please don\'t - fish are our friends)')
+    
 
 # create a list of dicts from a CSV
 recipes_list = convert_to_dict("recipe_info.csv")
@@ -21,7 +39,7 @@ def index():
 
 # second route
 
-@app.route('/recipe/<id>')
+@app.route('/recipe/<id>', methods=['GET', 'POST'])
 def detail(id):
     try:
         recipe_dict = recipes_list[int(id) - 1]
@@ -29,12 +47,57 @@ def detail(id):
         return f"<h1>Invalid recipe id: {id}</h1>"
     # a little bonus function, imported on line 2 above
     ord = make_ordinal( int(id) )
+    form = NameForm()
+    names = get_names(recipes_list)
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        if any(name.lower() in x for x in names):
+            # empty the form field
+            form.name.data = ""
+            id = get_id(recipes_list, name)
+            # redirect the browser to another route and template
+            return redirect( url_for('detail', id=id) )
+            #message = f"you searched for {name.lower()}. That recipe has id = {id}"
+        else:
+            message = f"you searched for {name.lower()}. That recipe is not in our database: {names}. The HECK is wrong with you"
     return render_template('recipe.html'
+                           , names=names
                            , recipe=recipe_dict
                            , ord=ord
                            , the_title=recipe_dict['Title']
+                           , form=form
                           )
 
+@app.route('/lookup', methods=['GET', 'POST']) #for more on flask forms: https://python-adv-web-apps.readthedocs.io/en/latest/flask_forms.htmls
+def lookup():
+    names = get_names(recipes_list)
+    form = NameForm()
+    message = ""
+    if form.validate_on_submit():
+        name = form.name.data
+        if any(name.lower() in x for x in names):
+            # empty the form field
+            form.name.data = ""
+            id = get_id(recipes_list, name)
+            # redirect the browser to another route and template
+            return redirect( url_for('detail', id=id) )
+            #message = f"you searched for {name.lower()}. That recipe has id = {id}"
+        else:
+            message = f"you searched for {name.lower()}. That recipe is not in our database: {names}. The HECK is wrong with you"
+    return render_template('lookup.html'
+                           , names=names
+                           , form=form
+                           , message=message)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 # keep this as is
 if __name__ == '__main__':
--- modules.py
+++ modules.py
@@ -51,10 +51,40 @@ def search_the_list(list):
     for k in list[0].keys():
         print(k)
 
-# run tryouts
-if __name__ == '__main__':
-    test_make_ordinal()
-    presidents_list = convert_to_dict("presidents.csv")
-    search_the_list(presidents_list)
-    print(make_ordinal(12))
-    print(make_ordinal(32))
+        
+        
+# retrieve all the names from the dataset and put them into a list
+def get_names(source):
+    names = []
+    for row in source:
+        # lowercase all the names for better searching
+        name = row["Title"].lower()
+        names.append(name)
+    return sorted(names)
+
+# find the row that matches the id in the URL, retrieve name and photo
+def get_recipe(source, id):
+    for row in source:
+        if id == str( row["id"] ):
+            name = row["name"]
+            photo = row["photo"]
+            # change number to string
+            id = str(id)
+            # return these if id is valid
+            return id, name, photo
+    # return these if id is not valid - not a great solution, but simple
+    return "Unknown", "Unknown", ""
+
+# find the row that matches the name in the form and retrieve matching id
+def get_id(source, name):
+    for row in source:
+        # lower() makes the string all lowercase
+        if name.lower() in row["Title"].lower():
+            id = row["id"]
+            # change number to string
+            id = str(id)
+            # return id if name is valid
+            return id
+    # return these if id is not valid - not a great solution, but simple
+    return "Unknown"
+    
